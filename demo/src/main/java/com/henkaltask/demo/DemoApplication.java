2467212c7bfe538effd9ac4491d6f76deae34035
govind-biradar <149034916+govind-biradar@users.noreply.github.com>
2024-06-25 11:23:42 +0530

    commit

--- demo/src/main/java/com/henkaltask/demo/DemoApplication.java
+++ demo/src/main/java/com/henkaltask/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 	
 	@GetMapping("/message")
 	public String message(){
-		return "Congrats ! your application deployed successfully in Azure Platform new version. !";
+		return "Congrats ! your application deployed successfully in Azure Platform last version. !";
 	}
 
 	public static void main(String[] args) {
